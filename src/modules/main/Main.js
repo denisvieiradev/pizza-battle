d031c3571fecaab0bd6ad9edde2eecd8f9e87eb5
Denis Vieira <denisvieira05@gmail.com>
2017-10-30 01:21:24 -0300

    hotfix/#19 Fix styles to implement correct new features
    -
    
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- src/modules/main/Main.js
+++ src/modules/main/Main.js
@@ -3,29 +3,26 @@ import { Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
+import './Main.scss'
+
+const { Header, Footer, Content } = Layout;
 
 const Main = () => (
-  <Layout
-    style={{
-      background: '#f8f8f8',
-      position: 'absolute',
-      overflow: 'hidden',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-    }}
-  >
+  <div>
     <Route exact path="/" component={Login} />
     <Route exact path="/signup" component={Signup} />
     <Route path="/home" component={Home} />
-  </Layout>
+  </div>
 );
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+  <Layout>
+    <Header>Header</Header>
+    <Content>
+      <h2>Home</h2>
+    </Content>
+    <Footer>Footer</Footer>
+  </Layout>
 )
 
 export default Main;
